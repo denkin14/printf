a6f06797b2b306a3ebf07331b766d6d7b929b99c
denkin14 <dennismuguongo@gmail.com>
2022-05-22 16:07:55 +0000

    truncated case functions

--- _printf.c
+++ _printf.c
@@ -1,111 +1,51 @@
 #include "main.h"
+#include <stdarg.h>
+#include <stdio.h>
+
 /**
- *_printf - printf
- *@format: const char pointer
- *Description: this functions implement some functions of printf
- *Return: num of characteres printed
- */
+  * _printf - produces outoput according to a format.
+  * @format: format specifier.
+  *
+  * Return: number of characters.
+  */
 int _printf(const char *format, ...)
 {
-	const char *string;
-	int cont = 0;
-	va_list arg;
+	int i, len_format = 0, charptr;
+	int *num = &charptr;
+	char *ch = (char *)num;
+	char *str;
 
-	if (!format)
-		return (-1);
+	va_list args;
 
-	va_start(arg, format);
-	string = format;
+	va_start(args, format);
 
-	cont = loop_format(arg, string);
+	while (format[len_format])
+		len_format++;
 
-	va_end(arg);
-	return (cont);
-}
-/**
- *loop_format - loop format
- *@arg: va_list arg
- *@string: pointer from format
- *Description: This function make loop tp string pointer
- *Return: num of characteres printed
- */
-int loop_format(va_list arg, const char *string)
-{
-	int i = 0, flag = 0, cont_fm = 0, cont = 0, check_per = 0;
-
-	while (i < _strlen((char *)string) && *string != '\0')
+	for (i = 0; i < len_format; i++)
 	{
-		char aux = string[i];
-
-		if (aux == '%')
+		if (format[i] == 'c')
 		{
-			i++, flag++;
-			aux = string[i];
-			if (aux == '\0' && _strlen((char *)string) == 1)
-				return (-1);
-			if (aux == '\0')
-				return (cont);
-			if (aux == '%')
-			{
-				flag++;
-			} else
-			{
-				cont_fm = function_manager(aux, arg);
-				if (cont_fm >= 0 && cont_fm != -1)
-				{
-					i++;
-					aux = string[i];
-					if (aux == '%')
-						flag--;
-					cont = cont + cont_fm;
-				} else if (cont_fm == -1 && aux != '\n')
-				{
-					cont += _putchar('%');
-				}
-			}
+			charptr = va_arg(args, int);
+			fprintf(stdout, ch);
 		}
-		check_per = check_percent(&flag, aux);
-		cont += check_per;
-		if (check_per == 0 && aux != '\0' && aux != '%')
-			cont += _putchar(aux), i++;
-		check_per = 0;
+
+		else if (format[i] == 's')
+		{
+			str = va_arg(args, char *);
+			fprintf(stdout, str);
+		}
+
+
+		else if (format[i] == '%')
+		{
+			fprintf(stdout, "%%");
+		}
+
+		else
+			break;
 	}
-	return (cont);
-}
-/**
- * check_percent - call function manager
- *@flag: value by reference
- *@aux: character
- *Description: This function print % pear
- *Return: 1 if % is printed
- */
-int check_percent(int *flag, char aux)
-{
-	int tmp_flag;
-	int cont = 0;
 
-	tmp_flag = *flag;
-	if (tmp_flag == 2 && aux == '%')
-	{
-		_putchar('%');
-		tmp_flag = 0;
-		cont = 1;
-	}
-	return (cont);
-}
-
-/**
- * call_function_manager - call function manager
- *@aux: character parameter
- *@arg: va_list arg
- *Description: This function call function manager
- *Return: num of characteres printed
- */
-
-int call_function_manager(char aux, va_list arg)
-{
-	int cont = 0;
-
-	cont = function_manager(aux, arg);
-	return (cont);
+	return (len_format - 1);
+	va_end(args);
 }
